c36751e176e164924e439110ab81344db8cba8f1
Jun2Hou <31128295+Jun2Hou@users.noreply.github.com>
2025-06-23 21:16:58 +0800

    Update repo_monitor.py

--- repo_monitor.py
+++ repo_monitor.py
@@ -20,7 +20,7 @@ else:
 target_repo = sys.argv[1].replace('_', '/')
 token = os.environ.get('GITHUB_TOKEN')
 # print('-----')
-print('-----' + target_repo)
+print(target_repo)
 
 # 版本文件路径
 VERSION_FILE = f"version_history_{safe_repo_name.replace('/', '_')}.json"
